8788ee37920092f29b1defb4cc111d6b47859053
Gustavo Silva <insiide123@gmail.com>
2021-09-09 00:51:28 -0300

    Pagination

--- backend/src/main/java/com/devsuperior/dsvendas/controllers/SellerController.java
+++ backend/src/main/java/com/devsuperior/dsvendas/controllers/SellerController.java
@@ -7,20 +7,20 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 import com.devsuperior.dsvendas.dto.SellerDTO;
-import com.devsuperior.dsvendas.services.SellerService;
+import com.devsuperior.dsvendas.service.SellerService;
 
 @RestController
 @RequestMapping(value = "/sellers")
 public class SellerController {
 
-	@Autowired
-	private SellerService service;
-	
-	@GetMapping
-	public ResponseEntity<List<SellerDTO>> findAll() {
-		List<SellerDTO> list = service.findAll();
-		return ResponseEntity.ok(list);
-	}
+    @Autowired
+    private SellerService service;
+
+    @GetMapping
+    public ResponseEntity<List<SellerDTO>> findAll() {
+        List<SellerDTO> list = service.findAll();
+        return ResponseEntity.ok(list);
+
+    }
 }
